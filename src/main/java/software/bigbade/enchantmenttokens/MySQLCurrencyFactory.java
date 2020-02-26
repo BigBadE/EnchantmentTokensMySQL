2d328f58bacc1656dae347e0ab643210e5064543
BigBadE <bigbade333@gmail.com>
2020-02-25 21:42:28 -0600

    Got MySQL working, fixed some other problems

--- src/main/java/software/bigbade/enchantmenttokens/MySQLCurrencyFactory.java
+++ src/main/java/software/bigbade/enchantmenttokens/MySQLCurrencyFactory.java
@@ -2,7 +2,6 @@ package software.bigbade.enchantmenttokens;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
-import software.bigbade.enchantmenttokens.utils.configuration.ConfigurationManager;
 import software.bigbade.enchantmenttokens.utils.EnchantLogger;
 import software.bigbade.enchantmenttokens.utils.configuration.ConfigurationType;
 import software.bigbade.enchantmenttokens.utils.currency.CurrencyFactory;
@@ -10,42 +9,59 @@ import software.bigbade.enchantmenttokens.utils.currency.CurrencyHandler;
 
 import java.sql.*;
 import java.util.logging.Level;
+import java.util.regex.Pattern;
 
 public class MySQLCurrencyFactory implements CurrencyFactory {
     private Connection connection;
-    private Statement statement;
     private String playerSection;
     private boolean loaded;
 
-    public MySQLCurrencyFactory(EnchantmentTokens main, ConfigurationSection section) {
+    public MySQLCurrencyFactory(ConfigurationSection section) {
         try {
-            String url = new ConfigurationType<String>("").getValue("database", section);
+            String url = "jdbc:" + new ConfigurationType<String>("").getValue("database", section);
             String username = new ConfigurationType<String>(null).getValue("username", section);
             String password = new ConfigurationType<String>(null).getValue("password", section);
 
             connection = DriverManager.getConnection(url, username, password);
 
-            statement = connection.createStatement();
-
             getDatabase(section);
 
             loaded = true;
         } catch (SQLException e) {
-            EnchantLogger.log(Level.SEVERE, "Could not open connection to database", e);
+            EnchantLogger.log("Could not open connection to database", e);
         }
     }
 
     private void getDatabase(ConfigurationSection section) throws SQLException {
         playerSection = new ConfigurationType<String>("players").getValue("section", section);
-        ResultSet result = statement.executeQuery("SELECT * FROM information_schema.TABLES WHERE TABLE_NAME = '" + playerSection + "' AND TABLE_SCHEMA = 'EnchantmentTokens' LIMIT 1;");
-        if (!result.next())
-            statement.execute("CREATE TABLE " + playerSection + "(gems LONG NOT NULL, uuid CHAR(36) NOT NULL);");
-        result.close();
+        if(Pattern.matches("[^a-zA-Z\\d\\s:]", playerSection)) {
+            loaded = false;
+            EnchantLogger.log(Level.SEVERE, "NON-ALPHANUMERIC CHARACTER DETECTED. POSSIBLE MYSQL INJECTION!");
+            return;
+        }
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM information_schema.TABLES WHERE TABLE_NAME = ? AND TABLE_SCHEMA = 'EnchantmentTokens' LIMIT 1;")) {
+            statement.setString(1, playerSection);
+            resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                try(PreparedStatement createTable = connection.prepareStatement("CREATE TABLE " + playerSection + "(gems LONG NOT NULL, uuid CHAR(36) NOT NULL);")) {
+                    createTable.executeUpdate();
+                }
+            }
+        } finally {
+            if(resultSet != null)
+                MySQLCurrencyHandler.safeClose(resultSet);
+        }
     }
 
     @Override
     public CurrencyHandler newInstance(Player player) {
-        return new MySQLCurrencyHandler(player, statement, playerSection);
+        try {
+            return new MySQLCurrencyHandler(player, connection, playerSection);
+        } catch (SQLException e) {
+            EnchantLogger.log("Problem initializing MySQL", e);
+        }
+        return null;
     }
 
     @Override
